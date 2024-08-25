a9b92f356c93c20577fd9ebf78bb71b6a8e3d0ad
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-25 23:21:19 +0700

    update LayoutDefault

--- src/layout/LayoutDefault/index.js
+++ src/layout/LayoutDefault/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import Footer from "../../components/Footer"
 export default function LayoutDefault() {
   return (
     <>
       <Outlet/>
+      <Footer/>
     </>
   )
 }
