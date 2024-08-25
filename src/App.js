70b95791f764c1e629a70acd21fc6b5b76416de8
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-25 15:27:14 +0700

    App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import Error404 from "./pages/Error404";
-import SignUp from "./pages/SignUp";
+import AllRoute from "./components/AllRoute";
 function App() {
   return (
    <>
-    <Routes>
-      <Route path="/signin" element={<SignIn/>}/>
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="*" element={<Error404/>}/>
-    </Routes>
+    <AllRoute/>
    </>
   );
 }
