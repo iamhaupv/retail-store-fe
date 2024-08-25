025b5eb52cd96d10b3fcebead726acacaaa1eff0
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-25 15:32:46 +0700

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
 import LayoutDefault from "../layout/LayoutDefault";
+import Error404 from "../pages/Error404";
+import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 export const routes = [
-    {
-        path: "/",
-        element: <LayoutDefault/>
-    }
-]
+  {
+    path: "/",
+    element: <LayoutDefault />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/signin", element: <SignIn /> },
+      { path: "/signup", element: <SignUp /> },
+      { path: "*", element: <Error404 /> },
+    ],
+  },
+];
