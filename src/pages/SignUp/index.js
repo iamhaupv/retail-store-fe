fc6b9fb3ca3ba20fac86f862352e32c7bbbfc416
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-25 22:48:09 +0700

    ContentRegisterLogin

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import HeaderRegisterLogin from "../../components/HeaderRegisterLogin";
+import ContentRegisterLogin from "../../components/ContentRegisterLogin";
+import FormRegister from "../../components/FormRegister";
 export default function SignUp() {
   return (
     <>
       <HeaderRegisterLogin text={"Đăng ký"} />
+      <ContentRegisterLogin component={FormRegister}/>
     </>
   );
 }
