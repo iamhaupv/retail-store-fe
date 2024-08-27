b58e71a8db6109cd8315e4554b618c74a07b0789
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-08-27 09:01:27 +0700

    FormLogin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import HeaderRegisterLogin from '../../components/HeaderRegisterLogin'
+import ContentRegisterLogin from '../../components/ContentRegisterLogin'
+import FormLogin from '../../components/FormLogin'
 
 export default function SignIn() {
   return (
     <>
-        <HeaderRegisterLogin/>
+        <HeaderRegisterLogin text={"Đăng nhập"}/>
+        <ContentRegisterLogin component={FormLogin}/>
     </>
   )
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import HeaderRegisterLogin from "../../components/HeaderRegisterLogin";
 import ContentRegisterLogin from "../../components/ContentRegisterLogin";
 import FormRegister from "../../components/FormRegister";
-import Footer from "../../components/Footer";
 export default function SignUp() {
   return (
     <>
       <HeaderRegisterLogin text={"Đăng ký"} />
       <ContentRegisterLogin component={FormRegister}/>
-      <Footer/>
     </>
   );
 }
