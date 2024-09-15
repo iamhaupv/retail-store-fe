bd1889d5fba0664b495ef7a5ab2bad69ea75f954
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-09-15 20:04:12 +0700

    update header

--- src/layout/LayoutDefault/index.js
+++ src/layout/LayoutDefault/index.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Footer from "../../components/Footer"
-import SideBar from '../../components/SideBar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
+import SideBar from "../../components/SideBar";
+import "./LayoutDefault.css"
 export default function LayoutDefault() {
   return (
     <>
-      <header>
-        <Outlet context={{ area: 'header' }} />
-      </header>
-      <div className="layout-main-content flex">
-        <NavSideBar/>
-        <Outlet context={{ area: 'main' }} />
+      <div className="container__layout">
+        <header>
+          <Outlet context={{ area: "header" }} />
+        </header>
+        <div className="layout-main-content flex">
+          <SideBar />
+          {/* <Outlet context={{ area: "main" }} /> */}
+        </div>
+        <footer>
+          <Footer />
+        </footer>
       </div>
-      <footer>
-        <Footer/>
-      </footer>
     </>
-  )
+  );
 }
