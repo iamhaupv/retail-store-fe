45d88bc9918bbcf183a492f32d2c4b624a62e446
NguyenThanhKhoa <nguyenthanhkhoa8888@gmail.com>
2024-09-24 22:59:56 +0700

    gần xong nút thêm sản phẩm vào kệ

--- src/components/InventoryProduct/index.js
+++ src/components/InventoryProduct/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ProductInventory from '../ProductInventory'
+import ListProductInventory from '../ListProductInventory'
 
 export default function InventoryProduct() {
   return (
+    <>
     <div>
       <select className="select-sm select-bordered w-52">
         <option disabled selected>Kệ</option>
@@ -14,7 +16,8 @@ export default function InventoryProduct() {
       </select>
       <div className='flex justify-between items-center pt-5'>
          <h4 className='font-bold text-xl w-32 ml-4'>Hàng tồn kho</h4>
-         <button className="btn btn-success text-white w-48">
+         <button className="btn btn-success text-white w-48"
+           onClick={()=>document.getElementById('AddInventoryProduct').showModal()}>
                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                </svg>
@@ -53,5 +56,61 @@ export default function InventoryProduct() {
         </table>
       </div>
     </div>
+
+    {/* Thêm Hàng vào kệ */}
+    <dialog id="AddInventoryProduct" className="modal">
+      <div className="modal-box w-3/5 h-3/4">
+        <h3 className="font-bold text-lg mb-6">Chọn mặt hàng</h3>
+        <div className='flex items-center'>
+           {/* Search Input  */}
+           <label className="input input-bordered w-52 h-12 flex  items-center gap-2">
+              <input type="text" className="grow" placeholder="Tên sản phẩm" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd" />
+              </svg>
+            </label>
+            {/* Product */}
+            <select className="select select-bordered w-28 ml-3 mr-3">
+              <option disabled selected>Loại sản phẩm</option>
+              <option>Đồ ăn</option>
+              <option>Thức uống</option>
+            </select>
+            {/* Brand */}
+            <select className="select select-bordered w-28 ">
+              <option disabled selected>Thương hiệu</option>
+              <option>KFC</option>
+              <option>Pepsi</option>
+            </select>
+        </div>
+        <div className="divider"/>
+
+        <div className="overflow-x-auto h-3/5">
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+              <ListProductInventory/>
+        </div>
+        <div className="modal-action">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn">Close</button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+    </>
   )
 }
---
+++ src/components/ListProductInventory/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function ListProductInventory() {
+  return (
+    <>
+        <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle h-12 w-12">
+                <img
+                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                  alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Hart Hagerty</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        </tr>
+    </>
+  )
+}
