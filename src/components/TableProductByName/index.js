fc03f9ec9accf6c01684bf6f4409eaa47b0ebd86
thkhoa9 <nguyenthanhkhoa8888@gmail.com>
2024-12-12 18:06:00 +0700

    xong phân quyen nv product

--- src/components/TableProductByName/index.js
+++ src/components/TableProductByName/index.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 import apiIsDisplay from "../../apis/apiIsDisplay";
 import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-export default function TableProductByName({ listProduct, reloadProducts }) {
-  const [products, setProducts] = useState(listProduct)
+export default function TableProductByName({
+  listProduct,
+  reloadProducts,
+  role,
+}) {
+  const [products, setProducts] = useState(listProduct);
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const handleChangeIsDisplay = async (pid, isDisplay) => {
@@ -18,7 +22,7 @@ export default function TableProductByName({ listProduct, reloadProducts }) {
     }
   };
   useEffect(() => {
-    setProducts(listProduct); 
+    setProducts(listProduct);
   }, [listProduct]);
   const handleDeleteProduct = () => {
     if (selectedProduct) {
@@ -36,7 +40,7 @@ export default function TableProductByName({ listProduct, reloadProducts }) {
   };
   return (
     <>
-    <ToastContainer/>
+      <ToastContainer />
       {products.map((product) => (
         <tr key={product._id} className="hover:bg-slate-100">
           <td>
@@ -73,9 +77,7 @@ export default function TableProductByName({ listProduct, reloadProducts }) {
             </h1>
           </td>
           <td>
-            <h1 className="whitespace-nowrap text-right">
-              {product.sold}
-            </h1>
+            <h1 className="whitespace-nowrap text-right">{product.sold}</h1>
           </td>
           <td>
             <div className="flex w-fit">
@@ -105,51 +107,55 @@ export default function TableProductByName({ listProduct, reloadProducts }) {
                   />
                 </svg>
               </button>
-              <Link state={{ product: product }} to="/update-product">
+              {role === "admin" && (
+                <Link state={{ product: product }} to="/update-product">
+                  <button
+                    className=" w-6 h-6 rounded-lg mr-2"
+                    style={{ backgroundColor: "#ebf3fe", outline: "" }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6"
+                      style={{ color: "#2f80ed" }}
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                      />
+                    </svg>
+                  </button>
+                </Link>
+              )}
+              {role === "admin" && (
                 <button
-                  className=" w-6 h-6 rounded-lg mr-2"
-                  style={{ backgroundColor: "#ebf3fe", outline: "" }}
+                  // onClick={() => handleChangeIsDisplay(product._id, false)}
+                  // id="btn__delete"
+                  onClick={() => handleOpenModalDelete(product)}
+                  className="w-6 h-6 rounded-lg "
+                  style={{ backgroundColor: "#feebe8", outline: "" }}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
                     stroke="currentColor"
-                    class="size-6"
-                    style={{ color: "#2f80ed" }}
+                    style={{ color: "#f13612" }}
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
                     />
                   </svg>
                 </button>
-              </Link>
-              <button
-                // onClick={() => handleChangeIsDisplay(product._id, false)}
-                // id="btn__delete"
-                onClick={() => handleOpenModalDelete(product)}  
-                className="w-6 h-6 rounded-lg "
-                style={{ backgroundColor: "#feebe8", outline: "" }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  style={{ color: "#f13612" }}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
+              )}
             </div>
             {/* Alert Delete */}
             <div></div>
@@ -235,8 +241,7 @@ export default function TableProductByName({ listProduct, reloadProducts }) {
                 {selectedProduct ? selectedProduct.price : ""} đ
               </h1>
 
-              <div className="card-actions justify-start">
-              </div>
+              <div className="card-actions justify-start"></div>
             </div>
           </div>
         </div>
